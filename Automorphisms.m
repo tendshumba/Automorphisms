7cda7dff50ae2dbe5faae57c010377cf19f31e71
Justin McInroy <j.f.mcinroy@gmail.com>
2023-05-02 15:37:39 +0100

    Added a gitignore

--- Automorphisms.m
+++ Automorphisms.m
@@ -21,14 +21,36 @@ intrinsic Algebra(A::ParAxlAlg) -> AlgGen
   return Algebra<BaseRing(A), Dimension(A) | A`mult>;
 end intrinsic;
 
-intrinsic AxesOrbitRepresentatives(A::ParAxlAlg) -> SetIndx
+intrinsic AxesOrbitRepresentatives(A::ParAxlAlg) -> SetIndx, List
   {
-  Returns a set of orbit representatives of axes of a ParAxlAlg coerced into an algebra.
+  Returns a set of orbit representatives of axes of a ParAxlAlg coerced into an algebra and a List of associative arrays giving the decompositions for each axis.
   }
   // This will check it is a complete algebra.
   AA := Algebra(A);
   
-  return {@ AA!A`axes[i]`id`elt : i in [1..#A`axes] @};
+  axes := {@ AA!A`axes[i]`id`elt : i in [1..#A`axes] @};
+  
+  eigs := A`fusion_table`eigenvalues;
+  Gr, gr := Grading(A`fusion_table);
+  require Order(Gr) eq 2: "The grading group must be of order 2";
+  
+  deccomps := {@@};
+  
+  keys := AssociativeArray();
+  keys["even"] := {@ e : e in eigs | e@gr eq Gr!1@};
+  keys["odd"] := {@ e : e in eigs | e@gr ne Gr!1@};
+
+  V := A`W;
+  
+  // We use a sequence, so there could be duplicate decompositions
+  decs := [* *];
+  for i in [1..#A`axes] do
+    D := AssociativeArray([* <k, sub<V | Basis(A`axes[i]``attr[{@k@}])> >
+                : k in keys[attr], attr in ["even", "odd"] *]);
+    Append(~decs, D);
+  end for;
+    
+  return axes, decs;
 end intrinsic;
 
 
@@ -61,23 +83,25 @@ end intrinsic;
 
 
 */
-IdentityLength := AssociativeArray();
 types := ["2A","2B","3A","3C","4A","4B","5A","6A"];
-identities_lengths := [(2^2*3)/5,2,(2^2*29)/(5*7),(2^5/11),4, 19/5,(2^5)/7,(3*17)/(2*5)];
-
-for i in [1..#types] do
-  IdentityLength[types[i]] := identities_lengths[i];
-end for;
+IdentityLength := AssociativeArray([* <"2A", (2^2*3)/5>,
+                                      <"2B", 2>,
+                                      <"3A", (2^2*29)/(5*7)>,
+                                      <"3C", (2^5/11)>,
+                                      <"4A", 4>,
+                                      <"4B", 19/5>,
+                                      <"5A", (2^5)/7>,
+                                      <"6A", (3*17)/(2*5)> *]);
 
 
 // FindAutNuanced
-intrinsic FindNewAxes(A::Alg, axes::SetIndx, decomp::SetIndx, form::AlgMatElt) -> SetIndx
+intrinsic FindNewAxes(axes::SetIndx, decomp::List, form::AlgMatElt) -> SetIndx
   {
-  The inputs are an axial algebra A with a set axes of (orbit representatives) of the axes and corresponding eigenspace decompositions together with a Frobenius form.
+  The inputs are a set axes of (orbit representatives) of the axes and corresponding eigenspace decompositions together with a Frobenius form.
   
   We perform the nuanced algorithm to find all axes.  
   }
-  require Parent(axes) eq A: "The axes are not in the algebra";
+  A := Universe(axes);
   F := BaseField(A);
   
   require Nrows(form) eq Ncols(form): "The form must be a square matrix.";
@@ -85,12 +109,15 @@ intrinsic FindNewAxes(A::Alg, axes::SetIndx, decomp::SetIndx, form::AlgMatElt) -
   require #axes eq #decomp: "There are not the same number of decompositions as axis representatives.";
   // Can change this later
   // What about other characteristics
-  require Type(decomp[1]) eq Assoc and forall{ d : d in decomp | Keys(d) subset {1,0,1/4,1/32}}: "The decompositions are not in the correct form.";
+  require forall{ d : d in decomp | Type(d) eq Assoc and Keys(d) subset {1,0,1/4,1/32} and &+[Dimension(d[k]) : k in Keys(d)] eq Dimension(A)}: "The decompositions are not in the correct form.";
   
-  found := [];
+
+  found := {@ @};
   for i in [1..#axes] do
     a := axes[i];
     dec := decomp[i];
+    
+    printf "Orbit number %o of %o\n", i, #axes;
     
     /*
     For one of our known axes a, we want to find a new axis b
@@ -100,38 +127,61 @@ intrinsic FindNewAxes(A::Alg, axes::SetIndx, decomp::SetIndx, form::AlgMatElt) -
     We use this to identify 1 and then the subalgebra.
     */
     // Change when decomposition algebra
-    A0 := decomp[0];
+    A0 := dec[0];
     
     for k in Keys(IdentityLength) do
       // Get the length of z
+      printf "  Eigenvalue is %o\n", k;
       len := IdentityLength[k] - 1;
       
       // if the type is 4A, then there are infinitely many such idempotents
       // Hence we need to add some extra relations
-      if k = "4A" then
-        A32 := decomp[1/32];
+      if k eq "4A" then
+        A32 := dec[1/32];
         R := PolynomialRing(F, Dimension(A0));
 				FF := FieldOfFractions(R);
 				AFF := ChangeRing(A, FF);
 				z := &+[R.i*AFF!A0.i : i in [1..Dimension(A0)]];
 				// Since z is in A0 and the fusion law is Seress, z A32 subset A32
 				// So we can restrict the action of the adjoint of z to A32
-				bas := Basis(A32)
+				bas := Basis(A32);
 				A32_vectorspace := VectorSpaceWithBasis(bas);
+				A32_vectorspace := ChangeRing(A32_vectorspace, FF);
 				adz := Matrix([ Coordinates(A32_vectorspace, Vector((AFF!b)*z)) : b in bas]);
 				
-				extra := [ Determinant(adz - (31/32)*IdentityMatrix(F, Dimension(A32))) ];
+				extra := [ Determinant(adz - (31/32)*IdentityMatrix(FF, Dimension(A32))) ];
 			else
-			  extra = [];
+			  extra := [];
 			end if;
 			
-  		idemps := FindAllIdempotents(A, A0: length:=len, form:=form, extra_rels:=extra);
+			t := Cputime();
+			print "  Finding idempotents";
+  		idems := FindAllIdempotents(A, A0: length:=len, form:=form, extra_rels:=extra);
+  		printf "  Found %o idempotents in %o secs\n", #idems, Cputime()-t;
   		
-      // Need to complete
+  		// From the idempotents found, find the algebra B = <<a,b>> and sift for Monster type axes.
+  		for z in idems do
+  		  // Since z = 1-a, we can find 1
+  		  one := a+z;
+  		  // The subalgebra B must be contained in the 1-eigenspace of one.
+  		  BB := Eigenspace(AdjointMatrix(one), 1);
+  		  
+  		  t := Cputime();
+  			print "  Finding idempotents";
+  		  possibles := FindAllIdempotents(A, BB: length:=1, form:=form);
+  		  printf "  Found %o idempotents in %o secs\n", #possibles, Cputime()-t;
+  		  
+  		  // check for the Monster fusion law
+  		  for y in possibles do
+  		    // COMPLETE THIS
+  		    Include(~found, y);
+  		  end for;
+  		end for;
+    end for;
+  end for;
   
   
-  
-  
+  return found;
 end intrinsic;
 
 
@@ -147,9 +197,65 @@ intrinsic FindAllIdempotents(A::Alg, U::ModTupFld: length:=false, form := false,
     form - the Frobenius form
     extra_rels - require the idempotent to satisfy extra relation(s).  These are given by multivariate polynomials in dim(U) variables corresponding to the basis of U.
   }
+  F := BaseRing(A);
+  n := Dimension(A);
+	m := Dimension(U);
+	
+  require m le n: "U must be a subspace of A";
+  if Type(form) ne BoolElt then
+    require ISA(Type(form), Mtrx): "The Frobenius form if given must be a matrix";
+    require Nrows(form) eq Ncols(form) and Nrows(form) eq n: "The matrix given for the Frobenius form must be a square matrix with dim(A) rows and columns";
+  end if;  
+  if Type(length) ne BoolElt then
+    // We have already checked it has the correct form.
+    require Type(form) ne BoolElt: "You need to provide a Frobenius form";
+    require IsCoercible(F, length): "The length of an axis must belong to the field of the algebra";
+  end if;
   
+  P := PolynomialRing(F, m);
+	FF := FieldOfFractions(P);
+	
+  if extra_rels ne [] then
+    require forall{ x : x in extra_rels | IsCoercible(FF, x)}: "The extra relations do not lie in the correct field";
+  end if;
   
+	// FF_to_P := hom<FF->P | [ P.i : i in [1..m]]>;
+	AF := ChangeRing(A, FF);
+	
+	// We set up a general element x
+	bas := Basis(U);
+	x := &+[ P.i*AF!bas[i] : i in [1..m]];
+	
+	// We add any extra relations coming from a length restriction
+	if Type(length) ne BoolElt then
+	  formF := ChangeRing(form, FF);
+	  extra_rels cat:= [ InnerProduct(Vector(x)*formF, Vector(x)) - length];
+	end if;
   
+  // form the ideal
+  I := ideal<P | Eltseq(x*x - x) cat extra_rels>;
+  
+  if Dimension(I) ge 1 then
+    print "The variety of idempotents is not zero-dimensional.  Try adding extra relations.";
+		return false;
+	end if;
+  
+  // Form the variety and check to see if we have all the solutions
+  var := Variety(I);
+  varsize := VarietySizeOverAlgebraicClosure(I);
+  if #var ne varsize then
+    print "Warning: there are additional idempotents over a field extension";
+  end if;
+  
+  // We need to coerce the variety elements back into the algebra
+  // We form a matrix whose rows are the elements of the variety (each row is the coefficients of the basis elements)
+  // and multiply by the matrix of basis elements
+  // idems := {@ A | r : r in Rows(Matrix([[v[i] : i in [1..m]]: v in var])*Matrix(bas)) @};
+  
+  // Do the simple coercion
+  idems := {@ A | &+[ v[i]*bas[i] : i in [1..m]]: v in var @};
+  
+  return idems;
 end intrinsic;
 
 
@@ -176,9 +282,9 @@ intrinsic JointEigenspaceDecomposition(L::SetIndx) -> Assoc
   Given an indexed set of axes L = \{ a_1, ..., a_n\}, decompose the algebra into joint eigenspaces for these axes.  Returns an associative array where the element A_lm_1(a_1) \cap ... \cap A_lm_n(a_n) has keys give by the set of eigenvalues \{ lm_1, ..., lm_n \}.
   }
   
-
+  /*
 	decomps:=AssociativeArray();
-	A:=Parent(lst[1]); /* why this must be really axial*/ 
+	A:=Parent(lst[1]); // why this must be really axial
 	eigs:=[1,0,1/4,1/32];
 	n:=#lst;
 	dims:=[];
@@ -194,6 +300,7 @@ intrinsic JointEigenspaceDecomposition(L::SetIndx) -> Assoc
 		end if; 
 	end for; 
 	return decomps;
+	*/
 end intrinsic;
 
 /*
