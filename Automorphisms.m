ac92879817775691b985046039a539c97e80341a
tendshumba <tendshumba@yahoo.com>
2024-02-02 08:51:13 +0700

    Update Automorphisms.m

--- Automorphisms.m
+++ Automorphisms.m
@@ -777,7 +777,7 @@ intrinsic IsAutomorphism(A::AlgGen, M::AlgMatElt: generators:=Basis(A)) -> BoolE
 	//ims := [generators[i]*M : i in [1..#generators]];
 	
 	// We use commutativity to reduce work
-	return forall{i : i in [1..#generators]| forall{ j : j in [i..#generators] |(gens[i]*gens[j])*M eq (gens[i]*gens[j])*M } } where gens is generators;
+	return forall{i : i in [1..#generators]| forall{ j : j in [i..#generators] |(gens[i]*M)*(gens[j]*M) eq (gens[i]*gens[j])*M } } where gens is generators;
 end intrinsic;
 /*
 
